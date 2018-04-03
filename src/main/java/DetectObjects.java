4f967bd221ef0ad404f918a23d69bd16030fdc51
root <root@gmail.com>
2018-04-03 11:33:56 -0400

    Added counter for clarity

--- src/main/java/DetectObjects.java
+++ src/main/java/DetectObjects.java
@@ -9,12 +9,15 @@ public class DetectObjects {
   public final static String savedModelPath = "./src/main/resources/saved_model";
 
   public static void main(String[] args) throws Exception {
-    int ch;
+    int ch, numRuns = 0 ;
     List<Tensor<?>> outputs = null;
 
     try {
 
       while ((ch = System.in.read()) != -1) {
+        System.out.println("##########################");
+        System.out.println(String.format("#         RUN %2d        #", ++numRuns));
+        System.out.println("##########################");
         try (SavedModelBundle model = SavedModelBundle.load(savedModelPath, "serve")) {
 
           try {
